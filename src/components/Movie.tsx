12ec70286ac07a117384a9fbb7a58f16bb6679de
alhareth alobaid <134533016+alharethalobaid@users.noreply.github.com>
2026-05-03 02:26:35 +0300

    Implement year comparison and conditional rendering
    Added state management for year comparison and conditional rendering based on the year.

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -1,5 +1,11 @@
-export default function Movie(props){
+import { createSignal, Match, Switch } from "solid-js";
 
+export default function Movie(props){
+  const [myear , setMyear] = createSignal(2000)
+
+  function updatemyear(e){
+    setMyear(e.target.value)
+   }
   return(
     <>
   <div class="card bg-base-100 w-96 shadow-sm">
@@ -10,7 +16,15 @@ export default function Movie(props){
     </figure>
     <div class="card-body">
       <h2 class="card-title">{props.item.title}</h2>
-      <p>{props.item.year}</p>
+      <Switch>
+      <Match when={props.item.year > props.myear}>
+              <p style={{ color: "red" }}>{props.item.year}</p>
+            </Match>
+            <Match when={props.item.year <= props.myear}>
+              <p>{props.item.year}</p>
+            </Match>
+          </Switch>
+
       <div class="card-actions justify-end">
       </div>
     </div>
